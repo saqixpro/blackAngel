c1ddb10d9df1a6acdaefb3ccbd81c477593d5395
Saqib Khan Afridi <saqib@itdunya.com>
2020-08-13 18:12:31 +0500

    username bug resolved

--- src/screens/username.jsx
+++ src/screens/username.jsx
@@ -37,17 +37,20 @@ export default class UserNameScreen extends Component {
   };
 
   handleSubmit = async () => {
-    if (username) {
+    if (this.state.username) {
       const userID = await firebase.auth().currentUser.uid;
-      setIsLoading(true);
+      this.setState({ loading: true });
       const user = await firebase
         .firestore()
         .collection("Users")
         .doc(userID)
         .get();
 
-      await user.ref.set({ username: username }, { merge: true });
-      setIsLoading(false);
+      await user.ref.set({ username: this.state.username }, { merge: true });
+
+      this.checkForUsername();
+
+      this.setState({ loading: false });
     }
   };
 
@@ -55,41 +58,51 @@ export default class UserNameScreen extends Component {
     return (
       <View style={styles.container}>
         {this.state.loading ? (
-          <Loading />
-        ) : (
-          <View>
-            <View style={styles.imageContainer}>
-              <Image
-                source={require("../assets/images/test.png")}
-                style={{ width: width / 1.5, height: width / 1.5 }}
-              />
-            </View>
-            <View style={styles.textContainer}>
-              <Text style={styles.message}>What Should We Call You ? </Text>
-              <TextInput
-                style={styles.input}
-                placeholder="username"
-                placeholderTextColor={colors.placeholderColor}
-                onChangeText={(text) => this.setState({ username: text })}
-                maxLength={11}
-              />
-              <TouchableOpacity
-                disabled={!this.state.appReady}
-                onPress={this.handleSubmit}
-                style={[
-                  styles.button,
-                  {
-                    backgroundColor: this.state.appReady
-                      ? colors.primary
-                      : colors.placeholderColor
-                  }
-                ]}
-              >
-                <Text style={styles.buttonText}>Submit</Text>
-              </TouchableOpacity>
-            </View>
+          <View
+            style={{
+              justifyContent: "center",
+              alignItems: "center",
+              position: "absolute",
+              top: 0,
+              bottom: 0,
+              left: 0,
+              right: 0,
+              backgroundColor: "transparent"
+            }}
+          >
+            <Loading />
           </View>
-        )}
+        ) : null}
+        <View style={styles.imageContainer}>
+          <Image
+            source={require("../assets/images/test.png")}
+            style={{ width: width / 1.5, height: width / 1.5 }}
+          />
+        </View>
+        <View style={styles.textContainer}>
+          <Text style={styles.message}>What Should We Call You ? </Text>
+          <TextInput
+            style={styles.input}
+            placeholder="username"
+            placeholderTextColor={colors.placeholderColor}
+            onChangeText={(text) => this.setState({ username: text })}
+            maxLength={11}
+          />
+          <TouchableOpacity
+            disabled={!this.state.appReady}
+            onPress={this.handleSubmit}
+            style={[
+              styles.button,
+              {
+                backgroundColor: this.state.appReady
+                  ? colors.primary
+                  : colors.placeholderColor
+              }
+            ]}
+          >
+            <Text style={styles.buttonText}>Submit</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     );
   }
